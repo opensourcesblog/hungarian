0a56f6e727c1f6a7eaf6222bdf8ecc06761715a1
Ole Kröger <o.kroeger@wikunia.de>
2019-04-03 16:18:50 +0200

    unmask and some times

--- hungarian.jl
+++ hungarian.jl
@@ -5,12 +5,12 @@ function run_hungarian(mat_original)
 
     # subtract col minimum
     for c=1:size(mat)[2]
-        mat[:,c] -= minimum(mat[:,c])
+        mat[:,c] .-= minimum(mat[:,c])
     end
 
     # subtract row minimum
     for r=1:size(mat)[1]
-        mat[r,:] -= minimum(mat[r,:])
+        mat[r,:] .-= minimum(mat[r,:])
     end
 
     nrows = size(mat)[1]
@@ -18,17 +18,34 @@ function run_hungarian(mat_original)
     rows_marked = zeros(Bool,nrows)
     cols_marked = zeros(Bool,ncols)
 
-    mask = ones(Bool, nrows, ncols)
+    ind_marked_rows = zeros(Int64, nrows)
+    ind_unmarked_rows = zeros(Int64, nrows)
+    ind_marked_cols = zeros(Int64, ncols)
+    ind_unmarked_cols = zeros(Int64, ncols)
 
-    col_unmask = ones(Bool, ncols)
-    row_mask = ones(Bool, nrows)
+    inner_while_total = 0.0
+    matzeros_total_time = 0.0
+    create_ind_vect_total_time = 0.0
+    find_min_total_time = 0.0
+    add_min_total_time = 0.0
+    sub_min_total_time = 0.0
+    
+    c_outer_while = 1
+    c_inner_while = 1
 
-    min_total = 0.0
     while true
+        c_outer_while += 1
         matching = get_matching(mat)
 
         if matching.cardinality == ncols
-            println("Total min time: ", min_total)
+            println("Total inner while time: ", inner_while_total)
+            println("Total matzeros_total_time: ", matzeros_total_time)
+            println("Total create_ind_vect_total_time: ", create_ind_vect_total_time)
+            println("Total find_min_total_time: ", find_min_total_time)
+            println("Total sub_min_total_time: ", sub_min_total_time)
+            println("Total add_min_total_time: ", add_min_total_time)
+            println("c_outer_while: ", c_outer_while)
+            println("c_inner_while: ", c_inner_while)
             return matching.match
         else
             sorted_matching = sort(matching.match)[1:matching.cardinality]
@@ -36,8 +53,8 @@ function run_hungarian(mat_original)
             # mark all rows which aren't matched
             mark_start = time()
             
-            rows_marked[:] = false
-            cols_marked[:] = false
+            rows_marked[:] .= false
+            cols_marked[:] .= false
             marked_col_ind = Vector{Int64}()
             marked_row_ind = Vector{Int64}()
             new_marked_row_ind = Vector{Int64}()
@@ -61,13 +78,16 @@ function run_hungarian(mat_original)
             end
         end
        
+        start_inner = time()
         changed = true
         while changed
+            c_inner_while += 1
+            
             changed = false
             new_marked_col_ind = Vector{Int64}()
             # mark cols
             in_start = time()
-            idx_not_marked_cols = find(x->!x,cols_marked)
+            idx_not_marked_cols = findall(x->!x,cols_marked)
             @inbounds for c in idx_not_marked_cols
                 @inbounds for r in new_marked_row_ind
                     @inbounds if mat[r,c] == 0
@@ -80,7 +100,7 @@ function run_hungarian(mat_original)
             end
 
             new_marked_row_ind = Vector{Int64}()
-            idx_not_marked_rows = find(x->!x,rows_marked)
+            idx_not_marked_rows = findall(x->!x,rows_marked)
             # mark new rows
             @inbounds for r in idx_not_marked_rows
                 @inbounds for c in new_marked_col_ind
@@ -94,41 +114,69 @@ function run_hungarian(mat_original)
                 end
             end
         end
+        inner_while_total += time()-start_inner
 
-        start_min = time()
+        start_timer = time()
+        ind_marked_rows_end = 0
+        ind_unmarked_rows_end = 0
+        for r=1:nrows
+            if rows_marked[r]
+                ind_marked_rows_end += 1
+                ind_marked_rows[ind_marked_rows_end] = r
+            else
+                ind_unmarked_rows_end += 1
+                ind_unmarked_rows[ind_unmarked_rows_end] = r
+            end
+        end
+        
+        ind_marked_cols_end = 0
+        ind_unmarked_cols_end = 0
+        for c=1:ncols
+            if cols_marked[c]
+                ind_marked_cols_end += 1
+                ind_marked_cols[ind_marked_cols_end] = c
+            else
+                ind_unmarked_cols_end += 1
+                ind_unmarked_cols[ind_unmarked_cols_end] = c
+            end
+        end
+        
+        @views ind_marked_rows_sub = ind_marked_rows[1:ind_marked_rows_end]
+        @views ind_unmarked_rows_sub = ind_unmarked_rows[1:ind_unmarked_rows_end]
+        @views ind_marked_cols_sub = ind_marked_cols[1:ind_marked_cols_end]
+        @views ind_unmarked_cols_sub = ind_unmarked_cols[1:ind_unmarked_cols_end]
+        create_ind_vect_total_time += time()-start_timer
+        
+        start_timer = time()
+        #minimum where !cols_marked but rows_marked
+        @inbounds @views min_val = minimum(mat[ind_marked_rows_sub,ind_unmarked_cols_sub])
+        find_min_total_time += time()-start_timer
+        
+        start_timer = time()
+        # subtract minimum from this mask
+        @inbounds for c in ind_unmarked_cols_sub
+            for r in ind_marked_rows_sub
+                mat[r,c] -= min_val
+            end
+        end
+        sub_min_total_time += time()-start_timer
 
-        mask[:,:] = true
-        mask[:,marked_col_ind] = false
-        row_mask[:] = true
-        row_mask[marked_row_ind] = false
-        mask[row_mask,:] = false
-    
-        min_val = minimum(mat[mask])
-        mat .-= min_val*mask
-
-        mask[:,:] = true
-        mask[marked_row_ind,:] = false
-        col_unmask[:] = true
-        col_unmask[marked_col_ind] = false
-        mask[:,col_unmask] = false
-        mat .+= min_val*mask
-
-        min_total += time() - start_min
+        start_timer = time()        
+        # add minimum where !rows_marked but cols_marked
+        @inbounds @views mat[ind_unmarked_rows_sub,ind_marked_cols_sub] .+= min_val
+        add_min_total_time += time()-start_timer
     end
 end
 
 function get_matching(mat)
     # create bi partite matching graph
-    zero_vals = find(x->x==0,mat)
+    zero_vals = findall(x->x==0,mat)
     ei = Vector{Int64}()
     ej = Vector{Int64}()
 
-    for i in zero_vals
-        # zero based
-        zi = i-1
-        zr = zi % size(mat)[1]
-        push!(ej,zr+1) # row
-        push!(ei,div((zi-zr),size(mat)[1])+1) # col
+    for yx in zero_vals
+        push!(ej,yx[1]) # row
+        push!(ei,yx[2]) # col
     end
     
     matching = bipartite_matching(ones(Int64,length(ei)), ei, ej)
